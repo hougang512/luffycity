c59c5b2e14cf9b91f4d1a936b0acf168b5861ce0
hougang512 <492858311@qq.com>
2019-08-23 19:53:09 +0800

    begin

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 new Vue({
   router,
   render: function (h) { return h(App) }
-}).$mount('#app')
+}).$mount('#app');
